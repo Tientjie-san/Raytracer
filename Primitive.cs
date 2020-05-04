713de498e75b7e1bc9f07d7fbeae623a4a70c4d0
Tientjie-san <63338063+Tientjie-san@users.noreply.github.com>
2020-05-04 23:04:54 +0200

    Update Primitive.cs

--- Primitive.cs
+++ Primitive.cs
@@ -1,11 +1,11 @@
-﻿using OpenTK;
+using OpenTK;
 
 namespace Template
 {
     public abstract class Primitive
     {
         public Vector3 position;
-
+        public float color;
         public abstract Intersection Intersect(Ray ray);
     }
 
