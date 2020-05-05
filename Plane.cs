18329fb0b8408f5a75cd2a1efa529b9b63c2f609
Tientjie-san <63338063+Tientjie-san@users.noreply.github.com>
2020-05-06 00:59:55 +0200

    intersection moet nog

--- Plane.cs
+++ Plane.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +9,16 @@ namespace Template
 {
     public class Plane : Primitive
     {
+        public Vector3 normal;
+        public Plane(Vector3 position, Vector3 normal, Vector3 color)
+        {
+            this.position = position;
+            this.normal = normal;
+            this.color = color;
+        }
         public override Intersection Intersect(Ray ray)
         {
-
+            return null;
         }
     }
 }
