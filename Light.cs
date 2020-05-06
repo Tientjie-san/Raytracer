cb3f6fdffb432c548e813698d8e2a31d9a50a1a0
Tientjie-san <63338063+Tientjie-san@users.noreply.github.com>
2020-05-06 02:15:48 +0200

    Update Light.cs

--- Light.cs
+++ Light.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using OpenTK;
 
 namespace Template
@@ -6,9 +6,24 @@ namespace Template
     public class Light
     {
         public Vector3 location;
-        public Light(Vector3 location)
+        public float brightness;
+        public Vector3 color;
+        public float ambientStrength;
+
+        public Light(Vector3 location, Vector3 color, float ambientStrength)
         {
             this.location = location;
+            this.color = color;
+            this.ambientStrength = ambientStrength;
+
+        }
+
+        public Vector3 ComputeColor(Vector3 objectcolor, Vector3 normal, Vector3 Lightdirection)
+        {
+            Vector3 ambient = ambientStrength * color;
+            float diff = Math.Max(Vector3.Dot(normal, Lightdirection), 0);
+            Vector3 diffuse = diff * color;
+            return (ambient +diffuse) * objectcolor;
         }
     }
 }
