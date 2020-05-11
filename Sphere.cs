50e7096dbf8dc330610b5fbf220e5bf4d8af8ffa
Sebasc98 <52129238+Sebasc98@users.noreply.github.com>
2020-05-11 14:48:53 +0200

    normal berekening intersectie gefixt, positie was nul

--- Sphere.cs
+++ Sphere.cs
@@ -6,14 +6,13 @@ namespace Template
 {
     public class Sphere : Primitive
     {
-        public float radius;
+
         public float r2;
-        public Vector3  center;
+
 
         public Sphere(Vector3 position ,float r, Vector3 c)
         {
-            center = position;
-            radius = r;
+            this.position = position;
             r2 = r * r;
             color = c;
             
@@ -24,7 +23,7 @@ namespace Template
         public override Intersection Intersect(Ray ray)
         {
           
-            Vector3 c = this.center - ray.origin; 
+            Vector3 c = this.position - ray.origin; 
             float distance = Vector3.Dot(c, ray.direction); 
             Vector3 q = c - distance * ray.direction; 
             float p2 = Vector3.Dot(q, q);
@@ -38,7 +37,7 @@ namespace Template
             {
                 Vector3 locatie = ray.origin + distance * ray.direction;
                 // normal at location of intersection
-                Vector3 normal = (locatie - position).Normalized();
+                Vector3 normal = (locatie - this.position).Normalized();
                 return new Intersection(this, locatie, normal, distance);
             }
             else return null;
